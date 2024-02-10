485f3a3fa490becda3aa3eb10ff7db1531cf7869
Debaloy Chatterjee <debaloy110019@gmail.com>
2024-02-10 21:51:08 +0530

    env line updated

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import 'dotenv/config'
-require('dotenv').config()
 
 import router from './routes/posts.js'
 
